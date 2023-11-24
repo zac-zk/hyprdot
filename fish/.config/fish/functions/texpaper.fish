719af6eadbbc83cdc5bd71b76405babb03746bd9
Juvenileone <kai.zuo@outlook.com>
2023-11-24 12:00:01 +0800

    timely commit

---
+++ fish/.config/fish/functions/texpaper.fish
@@ -0,0 +1,4 @@
+function texpaper --wraps='ranger ~/Documents/swarmrobotics/manuscripts/latex/' --description 'alias texpaper=ranger ~/Documents/swarmrobotics/manuscripts/latex/'
+  ranger ~/Documents/swarmrobotics/manuscripts/latex/ $argv
+        
+end
