30579be434e99c1395f431ca7e6945cde2517bff
Juvenileone <kai.zuo@outlook.com>
2023-11-28 12:00:01 +0800

    timely commit

--- fish/.config/fish/functions/start_proxy.fish
+++ fish/.config/fish/functions/start_proxy.fish
@@ -1,5 +1,5 @@
 function start_proxy
-    set -x https_proxy=http://127.0.0.1:20172
-    set -x http_proxy=http://127.0.0.1:20172
-    set -x all_proxy=http://127.0.0.1:20172
+    export https_proxy=http://127.0.0.1:20172
+    export http_proxy=http://127.0.0.1:20172
+    export all_proxy=http://127.0.0.1:20172
 end
